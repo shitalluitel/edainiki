00eeeaee6da07d08e4638ebf28c19d266024385a
shitalluitel <ctalluitel@gmail.com>
2020-03-02 01:05:02 +0545

    Vakra-Tunndda Maha-Kaaya Suurya-Kotti Samaprabha | Nirvighnam Kuru Me Deva Sarva-Kaaryessu Sarvadaa ||

---
+++ apps/common/management/commands/start_app.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+
+from django.conf import settings
+from django.core.management.templates import TemplateCommand
+
+APPS_DIR = settings.APPS_DIR
+
+TEMPLATE_BASE_DIR = 'scratch/app_template'
+
+
+class Command(TemplateCommand):
+    help = (
+        f"Creates new app on the location {APPS_DIR}."
+    )
+    missing_args_message = "You must provide an App name."
+
+    def handle(self, *args, **options):
+        app_name = options.pop('name')
+        _ = options.pop('directory')
+
+        print('\n**Initiating startapp command **\n')
+        target = os.path.join(APPS_DIR, app_name.lower())
+        if not os.path.exists(target):
+            os.mkdir(target)
+        print(f"Creating app with name {app_name} ...")
+        options.update({
+            'template': TEMPLATE_BASE_DIR
+        })
+
+        super(Command, self).handle('app', app_name, target, **options)
+        print("Successfully crated app.")
+        print(f"Target for new app in 'apps.{app_name}'")
+
+        print("** Copying html template files **")
+
+        html_templates_dir = os.path.join(target, 'templates')
+        if not os.path.exists(os.path.join(html_templates_dir, f'{ app_name }')):
+            os.mkdir(os.path.join(html_templates_dir, f'{ app_name }'))
+
+        html_files = os.listdir(html_templates_dir)
+
+        for file in html_files:
+            file_path = os.path.join(html_templates_dir, file)
+            if os.path.isfile(file_path):
+                shutil.copy(file_path, os.path.join(html_templates_dir, f'{app_name}'))
+
+        print("** Copying html template files completed successfully **")
